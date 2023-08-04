a0cea60ca6f956a06cdc5841455f26f0b7bffc81
nasrathf2001 <nasrath.f@iqhired.com>
2023-08-04 10:44:36 +0530

    CRUD USER IOT

---
+++ api/v1/users/edit_iot_users.php
@@ -0,0 +1,62 @@
+<?php
+require "../../../vendor/autoload.php";
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+include_once '../../../config/database.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../../../classes/v1/IotUsers.php';
+
+$jwt = $_SERVER['HTTP_ACCESS_TOKEN'];
+if($jwt){
+    try {
+
+        $decoded = JWT::decode($jwt,  new Key($secretkey, 'HS256'));
+
+        // Access is granted. Add code of the operation here
+
+        $database = new Database();
+        $db = $database->getConnection();
+
+        $item = new IotUsers($db);
+
+        $data = json_decode(file_get_contents("php://input"));
+
+        $this->cust_id = $_POST['cust_id'];
+        $this->edit_cust_name = $_POST['edit_cust_name'];
+        $this->edit_cust_email = $_POST['edit_cust_email'];
+        $this->edit_cust_fistname = $_POST['edit_cust_fistname'];
+        $this->edit_cust_lastname = $_POST['edit_cust_lastname'];
+        $this->edit_mobile = $_POST['edit_mobile'];
+        $this->edit_role = $_POST['edit_role'];
+        $this->edit_cust_profile_pic = $_POST['edit_cust_profile_pic'];
+        $this->edit_cust_address = $_POST['edit_cust_address'];
+
+        $sgPos = $item->editIotUsers();
+
+        if($sgPos != null){
+            echo json_encode(array("STATUS" => "Success" , "cust_id" => $sgPos));
+        } else{
+
+                http_response_code(401);
+            echo json_encode(array("message" => "Iot Device Update failed"));
+        }
+
+    }catch (Exception $e){
+
+        http_response_code(401);
+
+        echo json_encode(array(
+            "message" => "Access denied.",
+            "error" => $e->getMessage()
+        ));
+    }
+
+}
+?>
--- classes/v1/IotUsers.php
+++ classes/v1/IotUsers.php
@@ -44,6 +44,7 @@ class IotUsers{
         if ($dataRow == null || empty($dataRow)) {
             return null;
         } else {
+            $this->cust_id = $dataRow['cust_id'];
             $this->cust_name = $dataRow['cust_name'];
             $this->cust_email = $dataRow['cust_email'];
             $this->cust_fistname = $dataRow['cust_fistname'];
@@ -53,6 +54,37 @@ class IotUsers{
             $this->cust_profile_pic = $dataRow['cust_profile_pic'];
             $this->cust_address = $dataRow['cust_address'];
             $this->created_at = $dataRow['created_at'];
+            return $this;
+        }
+
+    }
+
+
+    public function editIotUsers()
+    {
+
+        $sqlQuery = "update " . $this->db_table . " SET cust_name = ? ,cust_email = ? ,cust_fistname = ? ,cust_lastname = ?,mobile = ? ,role = ? ,cust_profile_pic = ? ,cust_address = ?   where cust_id = '$this->cust_id'";
+
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute([$this->cust_name,$this->cust_email,$this->cust_fistname,$this->cust_lastname,$this->mobile,$this->role,$this->cust_profile_pic,$this->cust_address]);
+
+        $sqlQuery1 = "SELECT * FROM " . $this->db_table . " ORDER BY " . $this->db_table. ".cust_id DESC LIMIT 0,1";
+        $stmt = $this->conn->prepare($sqlQuery1);
+        $stmt->execute();
+        $dataRow = $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($dataRow == null || empty($dataRow)) {
+            return null;
+        } else {
+            $this->cust_id = $dataRow['cust_id'];
+            $this->edit_cust_name = $dataRow['edit_cust_name'];
+            $this->edit_cust_email = $dataRow['edit_cust_email'];
+            $this->edit_cust_fistname = $dataRow['edit_cust_fistname'];
+            $this->edit_cust_lastname = $dataRow['edit_cust_lastname'];
+            $this->edit_mobile = $dataRow['edit_mobile'];
+            $this->edit_role = $dataRow['edit_role'];
+            $this->edit_cust_profile_pic = $dataRow['edit_cust_profile_pic'];
+            $this->edit_cust_address = $dataRow['edit_cust_address'];
             return $this;
         }
 
