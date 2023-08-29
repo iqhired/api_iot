063bc4790fc387dc0c005773a1b771d5ea43af79
nasrathf2001 <nasrath.f@iqhired.com>
2023-08-29 15:55:17 +0530

    sensor Type CRUD

---
+++ api/v1/sensor_type/create_type.php
@@ -0,0 +1,59 @@
+<?php
+require "../../../vendor/autoload.php";
+use \Firebase\JWT\JWT;
+use \Firebase\JWT\Key;
+
+include_once '../../../config/database.php';
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+include_once '../../../classes/v1/sensorType.php';
+
+$jwt = $_SERVER['HTTP_ACCESS_TOKEN'];
+if($jwt){
+    try {
+
+        $decoded = JWT::decode($jwt,  new Key($secretkey, 'HS256'));
+
+        // Access is granted. Add code of the operation here
+
+        $database = new Database();
+        $db = $database->getConnection();
+
+        $item = new sensorType($db);
+
+        $data = json_decode(file_get_contents("php://input"));
+
+        $item->stype_name = empty($_POST["stype_name"])?null:$_POST["stype_name"];
+        $item->created_at = $_POST['created_at'];
+        $item->updated_at = $_POST['updated_at'];
+
+
+        $sgType = $item->createType();
+
+        if($sgType != null){
+            http_response_code(200);
+            echo json_encode(array("status" => "SUCCESS" , "stype_name" => $sgType));
+        } else{
+            http_response_code(401);
+            echo json_encode(array("message" => "Iot Device failed"));
+        }
+    }catch (Exception $e){
+        http_response_code(401);
+        $mess = '';
+        if($e->errorInfo[0] == '23000'){
+            $eD = explode('for key' , $e->errorInfo[2]);
+            $mess = $eD[0] . '.' . 'Check ' . str_replace( '_',' ', $eD[1] );
+        }
+        echo json_encode(array(
+            "status" => "ERROR",
+            "message" => $mess
+        ));
+    }
+
+}
+?>
---
+++ classes/v1/sensorType.php
@@ -0,0 +1,113 @@
+<?php
+class sensorType
+{
+    private $conn;
+    private $db_table = "sensor_type";
+
+
+    public $type_id;
+
+    public $stype_name;
+
+    public $created_at;
+    public $updated_at;
+
+    public $delete_check;
+
+    public function __construct($db)
+    {
+
+        $this->conn = $db;
+    }
+
+    public function createType()
+    {
+
+        $sqlQuery = "insert into " . $this->db_table . "(stype_name,created_at) values (?,?)";
+
+        $stmt = $this->conn->prepare($sqlQuery);
+        $resp = $stmt->execute([$this->stype_name, $this->created_at]);
+
+        $sqlQuery1 = "SELECT * FROM " . $this->db_table . " ORDER BY " . $this->db_table. ".type_id desc limit 0,1";
+        $stmt = $this->conn->prepare($sqlQuery1);
+        $stmt->execute();
+        $dataRow = $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($dataRow == null || empty($dataRow)) {
+            return null;
+        } else {
+            $this->stype_name = $dataRow['stype_name'];
+            $this->created_at = $dataRow['created_at'];
+            return $this;
+        }
+
+    }
+    public function editType()
+    {
+
+        $sqlQuery = "update " . $this->db_table . " SET stype_name  = ? ,updated_at = ?  where type_id = '$this->type_id'";
+
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute([$this->stype_name,$this->updated_at]);
+
+        $sqlQuery1 = "SELECT * FROM " . $this->db_table . " ORDER BY " . $this->db_table. ".type_id desc limit 0,1";
+        $stmt = $this->conn->prepare($sqlQuery1);
+        $stmt->execute();
+        $dataRow = $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($dataRow == null || empty($dataRow)) {
+            return null;
+        } else {
+            $this->stype_name = $dataRow['stype_name'];
+            $this->updated_at = $dataRow['updated_at'];
+            return $this;
+        }
+
+    }
+    public function deleteType()
+    {
+        $sqlQuery = "update " . $this->db_table . " SET is_deleted = 1  where type_id = ?";
+        // $sqlQuery = "delete from " . $this->db_table . " where device_id = ?";
+
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute([$this->delete_check]);
+
+        $sqlQuery1 = "SELECT * FROM " . $this->db_table . " ORDER BY " . $this->db_table. ".type_id desc limit 0,1";
+        $stmt = $this->conn->prepare($sqlQuery1);
+        $stmt->execute();
+        $dataRow = $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($dataRow == null || empty($dataRow)) {
+            return null;
+        } else {
+            $this->type_id = $dataRow['type_id'];
+            return $this;
+        }
+
+    }
+    public function delType()
+    {
+        $sqlQuery = "update " . $this->db_table . " SET is_deleted = 1  where type_id = ?";
+        $stmt = $this->conn->prepare($sqlQuery);
+        $stmt->execute([$this->type_id]);
+
+        $sqlQuery1 = "SELECT * FROM " . $this->db_table . " ORDER BY " . $this->db_table. ".type_id desc limit 0,1";
+        $stmt = $this->conn->prepare($sqlQuery1);
+        $stmt->execute();
+        $dataRow = $dataRow = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($dataRow == null || empty($dataRow)) {
+            return null;
+        } else {
+            $this->type_id = $dataRow['type_id'];
+            return $this;
+        }
+
+    }
+
+}
+
+
+
+
+
